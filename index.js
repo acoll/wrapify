331a44d667a728b1828f33290c163a88624da7ed
Adam <adamcoll.ac@gmail.com>
2015-10-13 10:22:54 -0400

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+var through = require('through2');
+var path = require('path');
+
+module.exports = function (file, config) {
+
+	var prefixes = [];
+	var suffixes = [];
+
+	// fix filename to be relative to module
+	file = file.replace(path.parse(path.resolve('.')).dir + path.sep, '');
+
+	config.wrappers.forEach(wrapper => {
+		if(file.match(wrapper.pattern)) {
+			if(wrapper.prefix)
+				prefixes.push(wrapper.prefix.replace(/\$filename/g, file) + '\n');
+			if(wrapper.suffix)
+				suffixes.push('\n' + wrapper.suffix.replace(/\$filename/g, file));
+		}
+	});
+
+	function transform (buf, enc, next) {
+		if(prefixes.length) {
+			this.push(prefixes.join('\n'));
+			prefixes = [];
+		}
+		this.push(buf);
+		next();
+	}
+
+	function flush (next) {
+		if(suffixes.length) this.push(suffixes.join('\n'));
+		next();
+	}
+
+	return through(transform, flush);
+
+};
